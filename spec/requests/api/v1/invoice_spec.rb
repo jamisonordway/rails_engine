377f1572ce7cc00840e93ca58a62c74e1778e182
JamisonOrdway <jamisonordway@gmail.com>
2018-06-25 19:01:20 -0600

    TESTS and implements Invoice Index and Invoice Show Endpoints

---
+++ spec/requests/api/v1/invoice_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe "Invoices Record" do
+    it "sends a list of invoices" do
+        create_list(:invoice, 3)
+
+        get '/api/v1/invoices'
+
+        expect(response).to be_successful
+
+        invoices = JSON.parse(response.body)
+    end 
+    it "can get one invoice by its id" do
+        id = create(:invoice).id
+
+        get "/api/v1/invoices/#{id}"
+
+        invoice = JSON.parse(response.body)
+        expect(response).to be_successful
+        expect(invoice["id"]).to eq(id)
+    end 
+end 
