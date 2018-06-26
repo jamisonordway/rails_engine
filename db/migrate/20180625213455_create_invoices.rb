e20ac1b6afe3e8ce3931e4fbec8141c1cd51f647
Sabrina Robinson <sabrinarob@gmail.com>
2018-06-25 18:27:05 -0600

    Imported CSVs

--- db/migrate/20180625213455_create_invoices.rb
+++ db/migrate/20180625213455_create_invoices.rb
@@ -1,8 +1,8 @@
 class CreateInvoices < ActiveRecord::Migration[5.2]
   def change
     create_table :invoices do |t|
-      t.references :customers, foreign_key: true
-      t.references :merchants, foreign_key: true
+      t.references :customer, foreign_key: true
+      t.references :merchant, foreign_key: true
       t.string :status
 
       t.timestamps
--- db/migrate/20180625213729_create_transactions.rb
+++ db/migrate/20180625213729_create_transactions.rb
@@ -3,7 +3,7 @@ class CreateTransactions < ActiveRecord::Migration[5.2]
     create_table :transactions do |t|
       t.references :invoice, foreign_key: true
       t.string :credit_card_number
-      t.string :results
+      t.string :result
 
       t.timestamps
     end
