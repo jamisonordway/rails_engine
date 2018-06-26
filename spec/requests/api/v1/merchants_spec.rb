f3e4ce4b5151f79043546c16a354fae8a5736242
Sabrina Robinson <sabrinarob@gmail.com>
2018-06-25 19:01:12 -0600

    Merchants index created with passing test

---
+++ spec/requests/api/v1/merchants_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe 'merchants API' do
+  it 'sends a list of merchants' do
+    create_list(:merchant, 3)
+
+    get '/api/v1/merchants'
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body)
+  end
+end
