7b4041cf805cf0ea17502cd975a3478139109c95
JamisonOrdway <jamisonordway@gmail.com>
2018-06-25 19:18:36 -0600

    TESTS and implements Update and Delete Invoice Record Endpoints

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -6,4 +6,22 @@ class Api::V1::InvoicesController < ApplicationController
     def show
         render json: Invoice.find(params[:id])
     end 
+
+    def create
+        render json: Invoice.create(invoice_params)
+    end 
+
+    def update
+        render json: Invoice.update(params[:id], invoice_params)
+    end 
+
+    def destroy
+        Invoice.delete(params[:id])
+    end 
+
+    private
+
+    def invoice_params
+        params.require(:invoice).permit(:customer_id, :merchant_id, :status)
+    end
 end 
--- spec/requests/api/v1/invoice_spec.rb
+++ spec/requests/api/v1/invoice_spec.rb
@@ -19,4 +19,39 @@ describe "Invoices Record" do
         expect(response).to be_successful
         expect(invoice["id"]).to eq(id)
     end 
+    it "can create a new invoice" do
+        id = create(:invoice).id
+        invoice_params = { customer_id: 1, merchant_id: 1, status: "paid"}
+
+        post "/api/v1/invoices", params: {invoice: invoice_params}
+        invoice = Invoice.last
+
+        assert_response :success
+        expect(response).to be_successful
+
+        expect(invoice.status).to eq(invoice_params[:status])
+    end 
+    it "can update an existing invoice" do
+        id = create(:invoice).id
+        previous_status = Invoice.last.status
+        invoice_params = { status: "Shipped" }
+
+        put "/api/v1/invoices/#{id}", params: {invoice: invoice_params}
+        invoice = Invoice.find_by(id: id)
+        
+        expect(response).to be_successful
+        expect(invoice.status).to_not eq(previous_status)
+        expect(invoice.status).to eq("Shipped")
+    end 
+    it "can destroy an invoice" do
+        invoice = create(:invoice)
+
+        expect(Invoice.count).to eq(1)
+        
+        delete "/api/v1/invoices/#{invoice.id}"
+
+        expect(response).to be_successful
+        expect(Invoice.count).to eq(0)
+        expect{Invoice.find(invoice.id)}.to raise_error(ActiveRecord::RecordNotFound)
+    end 
 end 
