f795747607c31ff2329732371e7af5c8563c4967
JamisonOrdway <jamisonordway@gmail.com>
2018-06-26 08:45:01 -0600

    implements invoice items record endpoints

--- spec/requests/api/v1/invoice_items_spec.rb
+++ spec/requests/api/v1/invoice_items_spec.rb
@@ -21,7 +21,7 @@ describe "Invoice Items Record" do
     end
     it "can create a new invoice item" do
         id = create(:invoice_item).id
-        invoice_item_params = { item_id: 1, invoice_id: 1, quantity: 1, unit_price: 1000 }
+        invoice_item_params = { item_id: 1, invoice_id: 1, quantity: 1 }
 
         post "/api/v1/invoice_items", params: {invoice_item: invoice_item_params}
         invoice_item = InvoiceItem.last
@@ -29,19 +29,19 @@ describe "Invoice Items Record" do
         assert_response :success
         expect(response).to be_successful
         
-        expect(invoice_item.unit_price).to eq(invoice_item_params[:unit_price])
+        expect(invoice_item.quantity).to eq(invoice_item_params[:quantity])
     end
     it "can update an existing invoice item" do
         id = create(:invoice_item).id 
-        previous_price = InvoiceItem.last.unit_price
-        invoice_item_params = { unit_price: 1100 }
+        previous_price = InvoiceItem.last.quantity
+        invoice_item_params = { quantity: 2 }
 
         put "/api/v1/invoice_items/#{id}", params: { invoice_item: invoice_item_params }
         invoice_item = InvoiceItem.find_by(id: id)
 
         expect(response).to be_successful
-        expect(invoice_item.unit_price).to_not eq(previous_price)
-        expect(invoice_item.unit_price).to eq(1100)
+        expect(invoice_item.quantity).to_not eq(previous_price)
+        expect(invoice_item.quantity).to eq(2)
     end 
     it "can destroy an invoice item" do
         invoice_item = create(:invoice_item)
