5355ac442279ae8d9f891610445c581f98145aca
Sabrina Robinson <sabrinarob@gmail.com>
2018-06-25 19:29:59 -0600

    CRUD for merchants complete with passing tests

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -2,4 +2,26 @@ class Api::V1::MerchantsController < ApplicationController
   def index
     render json: Merchant.all
   end
+
+  def show
+    render json: Merchant.find(params[:id])
+  end
+
+  def create
+    render json: Merchant.create(merchant_params)
+  end
+
+  def update
+    render json: Merchant.update(params[:id], merchant_params)
+  end
+
+  def destroy
+    Merchant.destroy(params[:id])
+  end
+
+  private
+
+  def merchant_params
+    params.require(:merchant).permit(:name)
+  end
 end
