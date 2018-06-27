29b5cbda647b91de618fa36d7d7d333bbdc9e0a6
JamisonOrdway <jamisonordway@gmail.com>
2018-06-27 15:43:12 -0600

    TESTS invoice relationship endpoints

---
+++ spec/requests/api/v1/relationship_endpoints/item_relationships_spec.rb
@@ -0,0 +1,35 @@
+describe 'items API' do
+    describe 'relationships' do
+      it "can return all items associated with a item" do
+        create(:item)
+        id = Item.last.id
+        id_2 = 2
+  
+        create(:item, id: id_2)
+        create_list(:item, 5, item_id: id)
+        create_list(:item, 9, item_id: id_2)
+        
+        get "/api/v1/items/#{id}/items"
+  
+        items = JSON.parse(response.body)
+        
+        expect(response).to be_successful
+        expect(items.count).to eq(5)
+      end
+      it "can return all invoices associated with a item" do
+        create(:item)
+        id = Item.last.id
+        id_2 = 2
+  
+        create(:item, id: id_2)
+        create_list(:invoice, 4, item_id: id)
+        create_list(:invoice, 8, item_id: id_2)
+  
+        
+        get "/api/v1/items/#{id}/invoices"
+        invoices = JSON.parse(response.body)
+  
+        expect(response).to be_successful
+        expect(invoices.count).to eq(4)
+      end
+    end 
