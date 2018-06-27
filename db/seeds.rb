3fb1dd26ff864ca56972f668cd5c708e0c6ae878
JamisonOrdway <jamisonordway@gmail.com>
2018-06-26 18:41:11 -0600

    merchant relationships wip

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,21 @@
-
+10.times do
+    Item.create!(
+      name: Faker::Commerce.product_name,
+      description: Faker::Hipster.words,
+      unit_price: Faker::Commerce.price,
+      merchant_id: rand(1..10),
+    )
+  end
+  
+  10.times do
+    Invoice.create!(invoice_id: rand(100000..999999))
+  end
+  
+  100.times do
+    InvoiceItem.create!(
+      item_id: rand(1..10),
+      invoice_id: rand(1..10),
+      unit_price: rand(100..10000),
+      quantity: rand(1..10),
+    )
+  end
