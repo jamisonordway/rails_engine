ac4614e38100aff5ce43ded2210362030e9cf408
JamisonOrdway <jamisonordway@gmail.com>
2018-06-27 11:56:58 -0600

    implements merchant relationships

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
         get '/find_all', to: 'search#index'
         get '/random', to: 'random#show'
         get '/:id/items', to: 'items#index'
+        get '/:id/invoices', to: 'invoices#index'
       end
 
       namespace :transactions do
